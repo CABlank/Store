afa5bf318c481807de6a06f33f35104db408c800
CABlank <54187031+CABlank@users.noreply.github.com>
2022-01-01 14:11:39 -0400

    first commit

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -4,19 +4,15 @@ const pool = require('../database');
 const helpers = require('../lib/helpers');
 
 
-passport.use('local', new LocalStrategy({
+passport.use('local.signup', new LocalStrategy({
     usernameField: 'username',
-    emailField:'email',
-    passwordField: 'password',
-    
-},  (req, username, email, password, done) => {
+    passwordField:'password',
+    passReqToCallback: true
+    }, async  (req, username, email, password, done) => {
     const newUser = {
         username,
-        email,
         password
     };
-
-
     const user =  pool.query('INSERT INTO users set ?', [newUser]);
         console.log(newUser);
 }));
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -4,24 +4,15 @@ const pool = require('../database');
 const passport = require('passport');
 
 
-
-
 router.get('/signup', (req, res) => {
     res.render('auth/signup');
 });
 
-
- router.post('/signup', passport.authenticate('local', {  
+ router.post('/signup', passport.authenticate('local.signup', {  
     successRedirect: '/profile',
     failureRedirect: '/signup',
     failureFlash: true
 }));
-
-
-
-
-
-
 
 router.get('/profile', (req, res) => {
     res.send('Profile')});
