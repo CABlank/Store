22eda37d336954346009ae4a1a24a259d3e35b3b
CABlank <54187031+CABlank@users.noreply.github.com>
2022-01-01 17:18:43 -0400

    register user solved
    I need added correct syntax on bcryptjs and add passport.serialize

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -3,22 +3,11 @@ const bcrypt = require('bcryptjs');
 const helpers = {};
 
 helpers.encryptPassword =  (password) => {
-    const salt =  bcrypt.genSalt(10);
-    const hash =  bcrypt.hash(password, salt);
-    return hash;
-};
-
-helpers.matchPassword=  (password, savedPassword) => {
-    bcrypt.compare(password, savedPassword, (err, isMatch) => {
-        if (err) throw err;
-        if (isMatch) {
-            return true;
-        } else {
-            return false;
-        }
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(password, salt, function(err, hash) {
+            const password =  pool.query('INSERT INTO users set ?', hash);
+        });
     });
 };
-
-
 
 module.exports = helpers;
--- src/lib/passport.js
+++ src/lib/passport.js
@@ -1,21 +1,36 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const pool = require('../database');
-const helpers = require('../lib/helpers');
+const bcrypt = require('bcryptjs');
 
 
 passport.use('local.signup', new LocalStrategy({
     usernameField: 'username',
     passwordField:'password',
     passReqToCallback: true
-    }, async  (req, username, email, password, done) => {
-    const newUser = {
-        username,
-        password
-    };
-    const user =  pool.query('INSERT INTO users set ?', [newUser]);
-        console.log(newUser);
-}));
+    },  async (req, username, password, done) => {
+        const newUser = {
+            username,
+            password
+        };
+        bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(password, salt, function(err, hash) {
+                newUser.password = hash;
+                pool.query('INSERT INTO users set ?', [newUser]);
+                return done(null, newUser);
+            });
+        });
+    }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
+
 
 
 
